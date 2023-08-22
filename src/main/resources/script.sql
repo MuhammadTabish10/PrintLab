272b1247a2d99ecfbe05fd43490f28a92960f954
Muhammad Tabish Rashid <muhammadtabish05@gmail.com>
2023-08-23 03:37:14 +0500

    Sql Script add

--- src/main/resources/script.sql
+++ src/main/resources/script.sql
@@ -1,67 +1,227 @@
---Data For Database
+-- MySQL dump 10.13  Distrib 8.0.34, for Win64 (x86_64)
+--
+-- Host: 127.0.0.1    Database: print_lab
+-- ------------------------------------------------------
+-- Server version	8.0.34
 
-INSERT INTO `customer` VALUES (1,'Tbcc','2023-08-22','Tabish','Active');
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
-INSERT INTO `orders` VALUES (2,138,45000,'Flyer',_binary '\0',3000,'DL','http',1);
+--
+-- Dumping data for table `customer`
+--
 
-INSERT INTO `paper_market_rates` VALUES (1,128,'2022-06-14','25\"x23\"',25,'Purchased from Abdullah paper mart','Glossy/ArtPaper',500,13600,_binary '\0',36),
-                                        (2,113,'2022-06-14','25\"x23\"',25,'Purchased from Abdullah paper mart','Glossy/ArtPaper',500,12000,_binary '\0',36),
-                                        (3,48,'2022-05-26','18\"x23\"',18,'Purchased from Abdullah paper mart','Carbonless',500,2750,_binary '\0',23);
+LOCK TABLES `customer` WRITE;
+/*!40000 ALTER TABLE `customer` DISABLE KEYS */;
+/*!40000 ALTER TABLE `customer` ENABLE KEYS */;
+UNLOCK TABLES;
 
-INSERT INTO `paper_size` VALUES (1,'18\"x23\"','Active'),
-                                (2,'25\"x36\"','Active'),
-                                (3,'23\"x36\"','Active'),
-                                (4,'8.5\"x12\"','Active');
+--
+-- Dumping data for table `orders`
+--
 
-INSERT INTO `press_machine` VALUES (1,400,400,_binary '\0','SOLNA'),
-                                   (2,220,400,_binary '','HEIDELBERG GTO-46'),
-                                   (3,220,400,_binary '\0','HEIDELBERG GTO-52');
+LOCK TABLES `orders` WRITE;
+/*!40000 ALTER TABLE `orders` DISABLE KEYS */;
+/*!40000 ALTER TABLE `orders` ENABLE KEYS */;
+UNLOCK TABLES;
 
-INSERT INTO `press_machine_size` VALUES (1,2,1,1),(2,4,2,1),(3,4,3,1),(4,1,4,1),
-                                        (5,2,1,2),(6,4,2,2),(7,4,3,2),(8,1,4,2),
-                                        (9,2,1,3),(10,4,2,3),(11,4,3,3),(12,1,4,3);
+--
+-- Dumping data for table `paper_market_rates`
+--
 
-INSERT INTO `product_definition` VALUES (1,_binary '','Flyer');
+LOCK TABLES `paper_market_rates` WRITE;
+/*!40000 ALTER TABLE `paper_market_rates` DISABLE KEYS */;
+INSERT INTO `paper_market_rates` VALUES (1,250,'2022-08-06','25\" x 36\"',25,'Junaid went there to get the rates.','Art card',100,6400,_binary '',36),(2,300,'2022-08-06','25\" x 36\"',25,'Junaid went there to get the rates.','Art card',100,7800,_binary '',36),(3,350,'2022-08-06','25\" x 36\"',25,'Junaid went there to get the rates.','Art card',100,9000,_binary '',36),(4,250,'2022-08-06','23\" x 36\"',23,'Junaid went there to get the rates.','Art card',100,5900,_binary '',36),(5,300,'2022-08-06','23\" x 36\"',23,'Junaid went there to get the rates.','Art card',100,7100,_binary '',36),(6,350,'2022-08-06','23\" x 36\"',23,'Junaid went there to get the rates.','Art card',500,8250,_binary '',36),(7,90,'2022-09-08','25\" x 36\"',25,'Junaid on WhatsApp','Art Paper / Matte',500,10000,_binary '',36),(8,250,'2022-08-06','25\" x 36\"',25,'Junaid went there to get the rates.','Bleach card',100,5700,_binary '',36),(9,300,'2022-08-06','25\" x 36\"',25,'Junaid went there to get the rates.','Bleach card',100,6800,_binary '',36),(10,350,'2022-08-06','25\" x 36\"',25,'Junaid went there to get the rates.','Bleach card',100,7949,_binary '',36),(11,250,'2022-08-06','23\" x 36\"',23,'Junaid went there to get the rates.','Bleach card',100,5210,_binary '',36),(12,300,'2022-08-06','23\" x 36\"',23,'Junaid went there to get the rates.','Bleach card',100,6250,_binary '',36),(13,350,'2022-08-06','23\" x 36\"',23,'Junaid went there to get the rates.','Bleach card',100,7300,_binary '',36),(14,48,'2022-05-27','18\" x 23\"',18,'Purchased from Abdullah paper mart','Caarbonless',500,2800,_binary '',23),(15,48,'2022-05-27','25\" x 36\"',25,'','Carbonless',500,5400,_binary '\0',36),(16,48,'2022-05-26','18\" x 23\"',18,'Purchased from Abdullah paper mart','Carbonless',500,2750,_binary '',23),(17,113,'2022-06-14','25\" x 36\"',25,'Abdullah paper mart via call','Glossy / Art Paper',500,12000,_binary '',36),(18,128,'2022-06-14','25\" x 36\"',25,'Abdullah paper mart via call','Glossy / Art Paper',500,13600,_binary '',36),(20,150,'2022-06-14','25\" x 36\"',25,'Abdullah paper mart via call','Glossy / Art Paper',500,16120,_binary '',36),(21,128,'2022-08-24','23\" x 36\"',23,'Wasi called paper mart','Matte Paper',500,13150,_binary '\0',36),(22,128,'2022-08-24','25\" x 36\"',25,'Wasi called paper mart','Matte Paper',500,14150,_binary '\0',36),(23,113,'2022-06-16','25\" x 36\"',25,'Abdullah paper mart via call','Matte Paper',500,12500,_binary '',36),(24,128,'2022-06-16','25\" x 36\"',25,'Abdullah paper mart via call','Matte Paper',500,13150,_binary '',36),(25,150,'2022-06-16','25\" x 36\"',25,'Abdullah paper mart via call','Matte Paper',500,16620,_binary '',36),(26,48,'2022-08-24','25\" x 36\"',25,'Junaid asked from Rafay','News Paper',500,2200,_binary '\0',36),(27,120,'2022-09-08','25\" x 36\"',25,'Junaid went there to get the rates.','Uncoated / Offset',500,12300,_binary '',36),(28,70,'2022-06-25','25\" x 36\"',25,'Junaid went there to get the rates.','Uncoated  / Offset',500,8650,_binary '',36),(29,80,'2022-06-25','25\" x 36\"',25,'Junaid went there to get the rates.','Uncoated / Offset',500,9900,_binary '',36),(30,100,'2022-06-25','25\" x 36\"',25,'Junaid went there to get the rates.','Uncoated / Offset',500,12300,_binary '',36),(31,70,'2022-06-25','23\" x 36\"',23,'Junaid went there to get the rates.','Uncoated / Offset',500,8050,_binary '',36),(32,80,'2022-06-25','23\" x 36\"',23,'Junaid went there to get the rates.','Uncoated / Offset',500,9200,_binary '',36),(33,100,'2022-06-25','23\" x 36\"',23,'Junaid went there to get the rates.','Uncoated / Offset',500,11500,_binary '',36),(34,70,'2022-05-27','25\" x 35.5\"',25,'Purchased from Abdullah paper mart','Uncoated / Offset',500,7820,_binary '',35),(35,100,'2022-09-08','8.5\" x 12\"',8.5,'','White Linen',500,3400,_binary '',12),(36,100,'2022-09-08','17\" x 24\"',17,'','Yellow Laid',500,8400,_binary '',24);
+/*!40000 ALTER TABLE `paper_market_rates` ENABLE KEYS */;
+UNLOCK TABLES;
 
-INSERT INTO `product_definition_field` VALUES (1,_binary '',1,3);
+--
+-- Dumping data for table `paper_size`
+--
 
-INSERT INTO `product_definition_process` VALUES (1,1,2,1);
+LOCK TABLES `paper_size` WRITE;
+/*!40000 ALTER TABLE `paper_size` DISABLE KEYS */;
+INSERT INTO `paper_size` VALUES (1,'18\"x23\"','Active'),(2,'17\"x24\"','Active'),(3,'23\"x36\"','Active'),(4,'25\"x36\"','Active'),(5,'8.5\"x12\"','Active'),(6,'20\"x30\"','Active');
+/*!40000 ALTER TABLE `paper_size` ENABLE KEYS */;
+UNLOCK TABLES;
 
-INSERT INTO `product_definition_selected_values` VALUES (1,'hi',1,1),(2,'hi',1,2);
+--
+-- Dumping data for table `press_machine`
+--
 
-INSERT INTO `product_field` VALUES (1,'2023-08-21','ProductName',4,'Active','TOGGLE'),
-                                   (2,'2023-08-21','PaperStock',2,'Active','DROPDOWN'),
-                                   (3,'2023-08-21','JobColor(Front)',3,'Active','DROPDOWN'),
-                                   (4,'2023-08-21','JobColor(Back)',4,'Active','DROPDOWN');
+LOCK TABLES `press_machine` WRITE;
+/*!40000 ALTER TABLE `press_machine` DISABLE KEYS */;
+INSERT INTO `press_machine` VALUES (1,220,400,_binary '\0','HEIDELBERG GTO-46'),(2,220,400,_binary '\0','HEIDELBERG GTO-52'),(3,400,400,_binary '\0','SOLNA'),(4,400,0,_binary '\0','SORK'),(5,520,750,_binary '\0','SORM'),(6,220,250,_binary '\0','ROTA(SPOT COLOR)');
+/*!40000 ALTER TABLE `press_machine` ENABLE KEYS */;
+UNLOCK TABLES;
 
+--
+-- Dumping data for table `press_machine_size`
+--
 
-INSERT INTO `product_field_values` VALUES (1,'Uncoated','Active',2),(2,'Offset','Active',2),
-                                          (3,'ArtPaper','Active',2),(4,'1','Active',3),
-                                          (5,'2','Active',3),(6,'3','Active',3),
-                                          (7,'4','Active',3),(8,'5','Active',3),
-                                          (9,'6','Active',3),(10,'1','Active',4),
-                                          (11,'2','Active',4),(12,'3','Active',4),
-                                          (13,'4','Active',4),(14,'5','Active',4),
-                                          (15,'6','Active',4);
+LOCK TABLES `press_machine_size` WRITE;
+/*!40000 ALTER TABLE `press_machine_size` DISABLE KEYS */;
+INSERT INTO `press_machine_size` VALUES (1,2,1,1),(3,4,3,1),(4,4,4,1),(5,1,5,1),(6,1,6,1),(7,2,1,2),(9,4,3,2),(10,4,4,2),(11,1,5,2),(13,1,1,3),(15,2,3,3),(16,2,4,3),(17,1,5,3),(20,1,1,4),(21,0,4,4),(22,0,3,4),(23,1,5,4),(26,0,3,5),(27,0,4,5),(28,1,1,5),(29,1,5,5),(32,4,3,6),(33,4,4,6),(34,2,1,6),(35,1,5,6);
+/*!40000 ALTER TABLE `press_machine_size` ENABLE KEYS */;
+UNLOCK TABLES;
 
-INSERT INTO `product_process` VALUES (1,'Lamination','Active'),(2,'Ctp','Active'),(3,'Slicing','Active');
+--
+-- Dumping data for table `product_definition`
+--
 
-INSERT INTO `setting` VALUES (1,'cuttingImpression','500'),(2,'cutting','70'),(3,'margin','50'),
-                             (4,'setupFee','5000');
+LOCK TABLES `product_definition` WRITE;
+/*!40000 ALTER TABLE `product_definition` DISABLE KEYS */;
+/*!40000 ALTER TABLE `product_definition` ENABLE KEYS */;
+UNLOCK TABLES;
 
-INSERT INTO `uping` VALUES (1,'DL'),(2,'LETTER'),(3,'A4'),(4,'A5');
+--
+-- Dumping data for table `product_definition_field`
+--
 
-INSERT INTO `uping_paper_size` VALUES (1,4,1,1),(2,8,2,1),(3,8,3,1),(4,1,4,1),(5,4,1,2),(6,8,2,2),
-                                      (7,8,3,2),(8,1,4,2),(9,4,1,3),(10,8,2,3),(11,8,3,3),(12,1,4,3),
-                                      (13,1,4,4);
+LOCK TABLES `product_definition_field` WRITE;
+/*!40000 ALTER TABLE `product_definition_field` DISABLE KEYS */;
+/*!40000 ALTER TABLE `product_definition_field` ENABLE KEYS */;
+UNLOCK TABLES;
 
-INSERT INTO `vendor` VALUES (1,'Gulistan','Contact1','03334215643','2023-02-12','MUTAHIR TRADERS','These are notes');
+--
+-- Dumping data for table `product_definition_process`
+--
 
-INSERT INTO `vendor_process` VALUES (1,'MATTE','These are new notesss',2.2,1,1),
-                                    (2,'GLOSSY','These are new notesss',1.8,1,1),
-                                    (3,'GLOSSY','These are new notesss',1.5,2,1),
-                                    (4,'MATTE','These are new notesss',1.8,2,1);
+LOCK TABLES `product_definition_process` WRITE;
+/*!40000 ALTER TABLE `product_definition_process` DISABLE KEYS */;
+/*!40000 ALTER TABLE `product_definition_process` ENABLE KEYS */;
+UNLOCK TABLES;
 
+--
+-- Dumping data for table `product_definition_selected_values`
+--
 
+LOCK TABLES `product_definition_selected_values` WRITE;
+/*!40000 ALTER TABLE `product_definition_selected_values` DISABLE KEYS */;
+/*!40000 ALTER TABLE `product_definition_selected_values` ENABLE KEYS */;
+UNLOCK TABLES;
 
+--
+-- Dumping data for table `product_field`
+--
 
+LOCK TABLES `product_field` WRITE;
+/*!40000 ALTER TABLE `product_field` DISABLE KEYS */;
+INSERT INTO `product_field` VALUES (1,'2023-08-22','JobColor(Front)',1,'Active','MULTIDROPDOWN'),(2,'2023-08-22','Paper Stock',3,'Active','MULTIDROPDOWN'),(3,'2023-08-22','Product Size',2,'Active','MULTIDROPDOWN'),(4,'2023-08-22','GSM',4,'Active','MULTIDROPDOWN'),(5,'2023-08-22','Sheet Size',5,'Active','MULTIDROPDOWN'),(6,'2023-08-22','Side Option',6,'Active','MULTIDROPDOWN'),(7,'2023-08-22','Imposition',7,'Active','MULTIDROPDOWN'),(8,'2023-08-22','JobColor(Back)',8,'Active','MULTIDROPDOWN');
+/*!40000 ALTER TABLE `product_field` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping data for table `product_field_values`
+--
+
+LOCK TABLES `product_field_values` WRITE;
+/*!40000 ALTER TABLE `product_field_values` DISABLE KEYS */;
+INSERT INTO `product_field_values` VALUES (1,'1','Active',1),(2,'2','Active',1),(3,'3','Active',1),(4,'4','Active',1),(5,'5','Active',1),(6,'6','Active',1),(7,'Offset','Active',2),(8,'DL','Active',3),(9,'113','Active',4),(10,'18\" x 23\"','Active',5),(11,'Single Sided','Active',6),(12,'Double Sided','Active',6),(13,'Applied','Active',7),(14,'Not Applied','Active',7),(15,'Not Applicable','Active',7),(16,'1','Active',8),(17,'2','Active',8),(18,'3','Active',8),(19,'4','Active',8),(20,'5','Active',8),(21,'6','Active',8);
+/*!40000 ALTER TABLE `product_field_values` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping data for table `product_process`
+--
+
+LOCK TABLES `product_process` WRITE;
+/*!40000 ALTER TABLE `product_process` DISABLE KEYS */;
+INSERT INTO `product_process` VALUES (1,'CTP','Active'),(2,'SLICING','Active');
+/*!40000 ALTER TABLE `product_process` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping data for table `roles`
+--
+
+LOCK TABLES `roles` WRITE;
+/*!40000 ALTER TABLE `roles` DISABLE KEYS */;
+/*!40000 ALTER TABLE `roles` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping data for table `setting`
+--
+
+LOCK TABLES `setting` WRITE;
+/*!40000 ALTER TABLE `setting` DISABLE KEYS */;
+/*!40000 ALTER TABLE `setting` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping data for table `uping`
+--
+
+LOCK TABLES `uping` WRITE;
+/*!40000 ALTER TABLE `uping` DISABLE KEYS */;
+INSERT INTO `uping` VALUES (1,'LETTER'),(2,'DL'),(3,'A5'),(4,'A4'),(5,'A3'),(6,'BUSINESS CARD'),(7,'PRESENTATION FOLDER');
+/*!40000 ALTER TABLE `uping` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping data for table `uping_paper_size`
+--
+
+LOCK TABLES `uping_paper_size` WRITE;
+/*!40000 ALTER TABLE `uping_paper_size` DISABLE KEYS */;
+INSERT INTO `uping_paper_size` VALUES (1,4,2,1),(2,8,3,1),(3,8,4,1),(4,4,1,1),(5,1,5,1),(6,1,6,1),(7,12,2,2),(8,24,3,2),(9,24,4,2),(10,12,1,2),(11,0,5,2),(12,1,6,2),(13,8,2,3),(14,16,3,3),(15,16,4,3),(16,8,1,3),(17,0,5,3),(18,4,2,4),(19,8,3,4),(20,8,4,4),(21,4,1,4),(22,1,5,4),(23,2,2,5),(24,4,3,5),(25,4,4,5),(26,2,1,5),(27,0,5,5),(28,96,4,6),(29,4,4,7);
+/*!40000 ALTER TABLE `uping_paper_size` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping data for table `user_roles`
+--
+
+LOCK TABLES `user_roles` WRITE;
+/*!40000 ALTER TABLE `user_roles` DISABLE KEYS */;
+/*!40000 ALTER TABLE `user_roles` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping data for table `users`
+--
+
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping data for table `vendor`
+--
+
+LOCK TABLES `vendor` WRITE;
+/*!40000 ALTER TABLE `vendor` DISABLE KEYS */;
+INSERT INTO `vendor` VALUES (1,'','','',NULL,'MUTAHIR TRADDERS','');
+/*!40000 ALTER TABLE `vendor` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping data for table `vendor_process`
+--
+
+LOCK TABLES `vendor_process` WRITE;
+/*!40000 ALTER TABLE `vendor_process` DISABLE KEYS */;
+INSERT INTO `vendor_process` VALUES (1,'MATTE',NULL,12,2,1);
+/*!40000 ALTER TABLE `vendor_process` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2023-08-22 10:47:38
