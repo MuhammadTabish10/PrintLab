4c9d37ce4e3126372247397b44dbe7d4994a19b0
Muhammad Tabish Rashid <muhammadtabish05@gmail.com>
2023-09-12 17:29:25 +0500

    Email added in Vendor

--- src/main/java/com/PrintLab/dto/VendorDto.java
+++ src/main/java/com/PrintLab/dto/VendorDto.java
@@ -21,6 +21,7 @@ public class VendorDto
     private LocalDate date;
     private String contactName;
     private String contactNumber;
+    private String email;
     private String address;
     private String notes;
 
