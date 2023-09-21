b5605a6106a0a43d134e4958d8abb990dadba21a
Abdul Ahad Ali Khan <abdul.khan@celltech-usa.net>
2023-09-21 12:14:28 -0700

    init

--- src/main/java/com/PrintLab/config/security/JwtUtil.java
+++ src/main/java/com/PrintLab/config/security/JwtUtil.java
@@ -12,7 +12,8 @@ import java.util.function.Function;
 
 @Service
 public class JwtUtil {
-    private String SECRET_KEY = "secret";
+
+    private String SECRET_KEY = "404E635266556A586E3272357538782F413F4428472B4B6250645367566B5970";
 
     public String extractUsername(String token) {
         return extractClaim(token, Claims::getSubject);
