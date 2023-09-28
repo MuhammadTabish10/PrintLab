75446c655d17fbf3d5a0ad8341804ff24a50df12
Muhammad Tabish Rashid <muhammadtabish05@gmail.com>
2023-09-28 17:58:02 +0500

    permissions backend complete

--- src/main/java/com/PrintLab/service/impl/RoleServiceImpl.java
+++ src/main/java/com/PrintLab/service/impl/RoleServiceImpl.java
@@ -4,7 +4,6 @@ import com.PrintLab.dto.RoleDto;
 import com.PrintLab.exception.RecordNotFoundException;
 import com.PrintLab.model.Permission;
 import com.PrintLab.model.Role;
-import com.PrintLab.model.VendorProcess;
 import com.PrintLab.repository.RoleRepository;
 import com.PrintLab.service.RoleService;
 import org.springframework.stereotype.Service;
@@ -63,32 +62,6 @@ public class RoleServiceImpl implements RoleService {
             throw new RecordNotFoundException(String.format("Role not found for id => %d", id));
         }
     }
-
-//    @Transactional
-//    @Override
-//    public RoleDto updateRole(Long id, RoleDto roleDto) {
-//        Role role = toEntity(roleDto);
-//        Optional<Role> optionalRole = roleRepository.findById(id);
-//
-//        if (optionalRole.isPresent()) {
-//            Role existingRole = optionalRole.get();
-//
-//            Set<Permission> existingPerValues = existingRole.getPermissions();
-//            Set<Permission> newPerValues = roleDto.getPermissions();
-//
-//            // Remove permissions that are present in existingPerValues but not in newPerValues
-//            existingPerValues.removeIf(existingPermission -> !newPerValues.contains(existingPermission));
-//
-//            // Add permissions that are present in newPerValues but not in existingPerValues
-//            newPerValues.removeAll(existingPerValues);
-//            existingPerValues.addAll(newPerValues);
-//
-//            Role updatedRole = roleRepository.save(existingRole);
-//            return toDto(updatedRole);
-//        } else {
-//            throw new RecordNotFoundException(String.format("Role not found for id => %d", id));
-//        }
-//    }
 
     @Transactional
     @Override
