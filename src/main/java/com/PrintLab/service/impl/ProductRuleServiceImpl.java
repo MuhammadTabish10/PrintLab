a5e75ea6fb81ebf0fa96f0b3544289417c7859b6
Muhammad Tabish Rashid <muhammadtabish05@gmail.com>
2023-10-31 19:59:06 +0500

    friendly name added in productRule paperStock

--- src/main/java/com/PrintLab/service/impl/ProductRuleServiceImpl.java
+++ src/main/java/com/PrintLab/service/impl/ProductRuleServiceImpl.java
@@ -145,6 +145,7 @@ public class ProductRuleServiceImpl implements ProductRuleService {
                     ProductRulePaperStock existingPrpsValue = existingValue.get();
 
                     existingPrpsValue.setPaperStock(newValue.getPaperStock());
+                    existingPrpsValue.setCustomerFriendlyName(newValue.getCustomerFriendlyName());
                     existingPrpsValue.setBrand(newValue.getBrand());
                     existingPrpsValue.setMadeIn(newValue.getMadeIn());
                     existingPrpsValue.setDimension(newValue.getDimension());
@@ -190,6 +191,7 @@ public class ProductRuleServiceImpl implements ProductRuleService {
                     ProductRulePaperStockDto productRulePaperStockDto = new ProductRulePaperStockDto();
                     productRulePaperStockDto.setId(prps.getId());
                     productRulePaperStockDto.setPaperStock(prps.getPaperStock());
+                    productRulePaperStockDto.setCustomerFriendlyName(prps.getCustomerFriendlyName());
                     productRulePaperStockDto.setBrand(prps.getBrand());
                     productRulePaperStockDto.setMadeIn(prps.getMadeIn());
                     productRulePaperStockDto.setDimension(prps.getDimension());
@@ -224,6 +226,7 @@ public class ProductRuleServiceImpl implements ProductRuleService {
                     ProductRulePaperStock productRulePaperStock = new ProductRulePaperStock();
                     productRulePaperStock.setId(prps.getId());
                     productRulePaperStock.setPaperStock(prps.getPaperStock());
+                    productRulePaperStock.setCustomerFriendlyName(prps.getCustomerFriendlyName());
                     productRulePaperStock.setBrand(prps.getBrand());
                     productRulePaperStock.setMadeIn(prps.getMadeIn());
                     productRulePaperStock.setDimension(prps.getDimension());
