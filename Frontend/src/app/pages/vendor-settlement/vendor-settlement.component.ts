fee667635c1437f7564b15f546eefb5018085870
abdulbasitstepway <abdulbasitstepway@gmail.com>
2023-11-29 21:33:03 +0500

    vendor settlement 2

--- Frontend/src/app/pages/vendor-settlement/vendor-settlement.component.ts
+++ Frontend/src/app/pages/vendor-settlement/vendor-settlement.component.ts
@@ -1,7 +1,7 @@
+import { DatePipe } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { MessageService } from 'primeng/api';
-import { VendorSettlement } from 'src/app/Model/vendorSettlement';
 import { VendorSettlementService } from 'src/app/services/vendor-settlement.service';
 
 @Component({
@@ -9,39 +9,52 @@ import { VendorSettlementService } from 'src/app/services/vendor-settlement.serv
   templateUrl: './vendor-settlement.component.html',
   styleUrls: ['./vendor-settlement.component.css']
 })
-export class VendorSettlementComponent implements OnInit{
+export class VendorSettlementComponent implements OnInit {
   editMode: boolean = false;
-  vendorSettlementRecords: VendorSettlement[] = [];
   idFromQueryParam: number = 0;
-  vendorSettlementArray: any = []
+  vendorSettlementArray: any[] = []
   visible!: boolean
-  
+
   constructor(
     private route: ActivatedRoute,
     private vendorSettlement: VendorSettlementService,
-    private messageService:MessageService
+    private messageService: MessageService,
+    private datePipe: DatePipe
   ) { }
-ngOnInit(): void {
-  this.route.queryParams.subscribe((param) => {
-    this.idFromQueryParam = +param['id'];
+  ngOnInit(): void {
+    this.route.queryParams.subscribe((param) => {
+      this.idFromQueryParam = +param['id'];
 
-  });
-  this.getVendorSettlementById(this.idFromQueryParam);
-}
+    });
+    this.getVendorSettlementById(this.idFromQueryParam);
+  }
 
-getVendorSettlementById(id: number){
-  debugger
-  this.vendorSettlement.getVendorSettlementById(id).subscribe(response => {
-    this.vendorSettlementArray =response;
-    console.log(this.vendorSettlementArray);
-  }, error => {
-    this.showError(error);
-    this.visible = true
-  
-})
-}
-showError(error:any) {
-  this.messageService.add({ severity: 'error', summary: 'Error', detail: error.error.error });
-}
+  getVendorSettlementById(id: number) {
+    this.vendorSettlement.getVendorSettlementById(id).subscribe(response => {
+      this.vendorSettlementArray.push(response);
+      console.log(this.vendorSettlementArray);
+      this.vendorSettlementArray.forEach((el: any) => {
+        const dateArray = el.dateAndTime.split(/[ :\-]/);
+
+        el.dateAndTime = new Date(
+          parseInt(dateArray[0], 10),
+          parseInt(dateArray[1], 10) - 1,
+          parseInt(dateArray[2], 10),
+          parseInt(dateArray[3], 10),
+          parseInt(dateArray[4], 10),
+          parseInt(dateArray[5], 10)
+        );
+
+        el.dateAndTime = this.datePipe.transform(el.dateAndTime, 'EEEE, MMMM d, yyyy, h:mm a');
+      });
+    }, error => {
+      this.showError(error);
+      this.visible = true
+
+    })
+  }
+  showError(error: any) {
+    this.messageService.add({ severity: 'error', summary: 'Error', detail: error.error.error });
+  }
 
 }
