aae911d8a9a1f156af0350af2e584f7dc8352a24
Haresh Lohana <hareshlohana50@gmail.com>
2024-05-08 21:27:29 +0500

    Update Method For Job

--- src/main/java/com/PrintLab/service/impl/ProductionJobServiceImpl.java
+++ src/main/java/com/PrintLab/service/impl/ProductionJobServiceImpl.java
@@ -86,6 +86,14 @@ public class ProductionJobServiceImpl implements ProductionJobService {
 //            updateProcessList(productionJob, productionJobDto);
             updateProcessedDetails(productionJob, productionJobDto);
 //            updateProofs(productionJob, productionJobDto);
+
+            productionJob.setSizeCategory(productionJobDto.getSizeCategory());
+            productionJob.setSize(productionJobDto.getSize());
+            productionJob.setTimeStamp(productionJobDto.getTimeStamp());
+            productionJob.setCreatedBy(productionJobDto.getCreatedBy());
+            productionJob.setStatus(productionJobDto.getStatus());
+            productionJob.setCreatedBy(productionJobDto.getCreatedBy());
+
             ProductionJob updatedProductionJob = jobRepository.save(productionJob);
             return mapper.toProductionJobDto(updatedProductionJob);
         }
